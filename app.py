2b88a21b7b71a359c860d80611ce3cd6f7e80db4
Farooq cherukuri <chfarooq2001@gmail.com>
2026-08-15 15:08:19 +0530

    Update MySQL connection for cloud database

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 #connect to MySQL
 db = mysql_connector.connect(
     host = os.getenv('DB_HOST'),
+    port=int(os.getenv("DB_PORT")),
     user = os.getenv('DB_USER'),
     password = os.getenv('DB_PASSWORD'),
     database = os.getenv("DB_NAME")
